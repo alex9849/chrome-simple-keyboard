dbc587c922dee9a9353e82e810fe1bcd726f6144
alex9849 <alexander@liggesmeyer.net>
2025-04-20 10:00:50 +0200

    improved language label

--- src/options.js
+++ src/options.js
@@ -1,3 +1,52 @@
+const languages = [
+    { value: "arabic", label: "Arabic" },
+    { value: "assamese", label: "Assamese" },
+    { value: "armenianEastern", label: "Armenian Eastern" },
+    { value: "armenianWestern", label: "Armenian Western" },
+    { value: "balochi", label: "Balochi" },
+    { value: "belarusian", label: "Belarusian" },
+    { value: "bengali", label: "Bengali" },
+    { value: "brazilian", label: "Brazilian" },
+    { value: "burmese", label: "Burmese" },
+    { value: "chinese", label: "Chinese" },
+    { value: "czech", label: "Czech" },
+    { value: "english", label: "English" },
+    { value: "farsi", label: "Farsi" },
+    { value: "french", label: "French" },
+    { value: "georgian", label: "Georgian" },
+    { value: "german", label: "German" },
+    { value: "gilaki", label: "Gilaki" },
+    { value: "greek", label: "Greek" },
+    { value: "hebrew", label: "Hebrew" },
+    { value: "hindi", label: "Hindi" },
+    { value: "hungarian", label: "Hungarian" },
+    { value: "italian", label: "Italian" },
+    { value: "japanese", label: "Japanese" },
+    { value: "kannada", label: "Kannada" },
+    { value: "korean", label: "Korean" },
+    { value: "kurdish", label: "Kurdish" },
+    { value: "macedonian", label: "Macedonian" },
+    { value: "malayalam", label: "Malayalam" },
+    { value: "nigerian", label: "Nigerian" },
+    { value: "nko", label: "Nko" },
+    { value: "norwegian", label: "Norwegian" },
+    { value: "odia", label: "Odia" },
+    { value: "polish", label: "Polish" },
+    { value: "punjabi", label: "Punjabi" },
+    { value: "russian", label: "Russian" },
+    { value: "russianOld", label: "Russian Old" },
+    { value: "sindhi", label: "Sindhi" },
+    { value: "spanish", label: "Spanish" },
+    { value: "swedish", label: "Swedish" },
+    { value: "telugu", label: "Telugu" },
+    { value: "thai", label: "Thai" },
+    { value: "turkish", label: "Turkish" },
+    { value: "ukrainian", label: "Ukrainian" },
+    { value: "urdu", label: "Urdu" },
+    { value: "urduStandard", label: "Urdu Standard" },
+    { value: "uyghur", label: "Uyghur" }
+];
+
 
 // Saves options to chrome.storage
 function save_options() {
@@ -17,18 +66,11 @@ function save_options() {
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-    const languages = [
-        "arabic", "assamese", "armenianEastern", "armenianWestern", "balochi", "belarusian", "bengali", "brazilian",
-        "burmese", "chinese", "czech", "english", "farsi", "french", "georgian", "german", "gilaki", "greek", "hebrew",
-        "hindi", "hungarian", "italian", "japanese", "kannada", "korean", "kurdish", "macedonian", "malayalam",
-        "nigerian", "nko", "norwegian", "odia", "polish", "punjabi", "russian", "russianOld", "sindhi", "spanish",
-        "swedish", "telugu", "thai", "turkish", "ukrainian", "urdu", "urduStandard", "uyghur"
-    ]
     const languageNode = document.getElementById('keyboard-layout')
     for(const language of languages) {
         const option = document.createElement("option");
-        option.value = language
-        option.text = language
+        option.value = language.value
+        option.text = language.label
         languageNode.appendChild(option)
     }
 
